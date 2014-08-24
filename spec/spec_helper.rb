5d20a7a903ca6b4c708a0a2f5c7f6eb008d4611b
Nima Shariatian <nima@onevio.org>
2014-08-24 14:14:58 -0400

    adds: new rails_helper to install rspec3, updates: spec_helper for rspec3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,40 +1,4 @@
-# Run Coverage report
-require 'simplecov'
-require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start do
-  add_filter 'spec/dummy'
-  add_group 'Controllers', 'app/controllers'
-  add_group 'Helpers', 'app/helpers'
-  add_group 'Mailers', 'app/mailers'
-  add_group 'Models', 'app/models'
-  add_group 'Views', 'app/views'
-  add_group 'Libraries', 'lib'
-end
-
-# Configure Rails Environment
-ENV['RAILS_ENV'] = 'test'
-
-require File.expand_path('../dummy/config/environment.rb',  __FILE__)
-
-require 'rspec/rails'
-require 'ffaker'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
-
-# Requires factories defined in spree_core
-require 'spree/testing_support/factories'
-
-# Requires factories defined in lib/spree_zero_stock_products/factories.rb
-require 'spree_zero_stock_products/factories'
-
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
 
   # == Mock Framework
   #
@@ -45,11 +9,18 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
   config.color = true
+  config.formatter = :documentation
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.use_transactional_fixtures = true
+  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
+  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
+  # to setup a test will be unavailable to the browser, which runs under a separate server instance.
+  config.use_transactional_fixtures = false
 
-  config.fail_fast = ENV['FAIL_FAST'] || false
+  config.infer_spec_type_from_file_location!
+
+  config.fail_fast = true
+  config.order = "random"
 end
